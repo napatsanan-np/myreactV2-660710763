e17c9861f0a501ffb353f20766f538658f1dd184
Napatsanan <deesamer_n@su.ac.th>
2025-09-29 05:41:58 +0000

    MyReactV2-lab

---
+++ mypoem/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import './index.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <header className="bg-white shadow-md rounded p-8">
+        <h1 className="text-2xl font-bold text-bookstore-primary">สวัสดี react</h1>
+        <p className="mt-4 text-gray-600">ยินดีต้อนรับสู่โลกของ React ของคุณ!</p>
+        <button className="mt-6 px-4 py-2 bg-green-500 text-white rounded hover:bg-blue-600">เริ่มต้น</button>
+        <p>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
